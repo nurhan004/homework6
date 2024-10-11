a39c95d469b5efb5b56878cdab17fc9a989674bb
nurhan004 <sharshenbekov777@gmail.com>
2024-10-12 01:27:54 +0600

    первый коммит

---
+++ script.js
@@ -0,0 +1,96 @@
+//ЦИКЫЛ
+// while,do while,for
+
+// while(условия) {
+//     //код
+
+// }
+// let num = 1
+// while(num <= 100) {
+//     console.log(num);
+//     num ++
+    
+// }
+
+
+
+
+
+
+// do {
+//     //code
+// } while (усдовия) 
+
+
+
+
+// var num = 0
+
+// do {
+//     console.log(num);
+//     num++;
+    
+// } while(num <=30)
+// var num 
+
+// do {
+//   prompt ("жаз")
+// } while(num = prompt)
+
+
+// for (начадо; условия;шаг) {
+
+// }
+
+// for (let i=0; i<=10;i++) {
+//     console.log(i);
+    
+// }
+
+
+// for (let i=10;i>=0;i--){
+//     console.log(i);
+    
+// }
+
+// for (let i = 2; i <= 50; i++) {
+//     if (i % 2 == 0 )  {
+//      console.log(i);
+     
+//     }
+//   }
+
+
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 === 0) {
+//    console.log(i);
+   
+//   }else{
+//       console.log(`${i} Так сан`);
+      
+//   }
+// }
+
+
+
+
+
+// let num 
+
+// do {
+//      num = prompt('Ввдите число больше 10')
+// } while (num <= 10)
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+        console.log("кока-кола"); 
+    } else if (i % 3 === 0) {
+        console.log("кока"); 
+    } else if (i % 5 === 0) {
+        console.log("кола"); 
+    } else {
+        console.log(i); 
+    }
+}
+
